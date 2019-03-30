bd322b869ce73360d27b44f52ac32f3ffc7670bb
kinming92 <kinming.klm@gmail.com>
2019-03-29 19:10:16 -0700

    handle when b == 0

--- long_div/sub_8bit.cpp
+++ long_div/sub_8bit.cpp
@@ -19,9 +19,9 @@ int main() {
   cout << "b: " << bitset<24>(b) << endl;
   cout << "result: "<< bitset<24>(result)<<endl;
 
-  uint8_t a_0 = 0x00;
+  uint8_t a_0 = 0x0d;
   uint8_t a_1 = 0x00;
-  uint8_t a_2 = 0xd0;
+  uint8_t a_2 = 0x00;
 
   uint8_t b_0 = 0x0c;
   uint8_t b_1 = 0x00;
@@ -35,26 +35,28 @@ int main() {
 
   temp = ~(temp);
 
+  if(b_0 == 0 && b_1 == 0 && b_2 == 0){
+    //return because a - 0 = a
+    return 0;
+  }
   //a - b
-  if( a_0 <= b_0){
-    if(a_1 <= b_1){
-      // no borrow out from a_1
-      if(b_1 != 0){
-          a_2 = a_2 + temp;
-          a_2 += 1;
+  if( a_0 <= b_0 && a_1 <= b_1){
+      // need to borrow from a_2
+      if( b_1 != 0){
+        a_2 = a_2 + temp;
+        a_2 += 1;
+        // need to borrow from a_2 but a_1 is zero
+        if(a_1 == 0 && b_0 != 0){
+          a_1 = ~a_1;
         }
-
-      if(a_1 != 0 || b_1 != 0){
-        a_1 = ~a_1;
       }
       
-    }else{ // a_1 > b_1
+  }else if ( a_0 <= b_0 && a_1 > b_1){ // a_1 > b_1
       //there is a borrow out from a_0
       if( b_0 != 0){
         a_1 = a_1 + temp;
         a_1 += 1; 
       }
-    }
   }
   temp = ~ ( b_0 );
   a_0 = a_0 + temp;
@@ -67,6 +69,7 @@ int main() {
   temp = ~(b_2);
   a_2 += temp;
   a_2 += 1;
+
   cout << "result: "<< bitset<8>(a_2) << " " << bitset<8>(a_1) << " " << bitset<8>(a_0) <<endl;
 
   return 0;
